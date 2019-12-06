bf00a7b7831bcfd93848c75a69e2958d8b54763a
Mike Lynch <mjlynch2@gmail.com>
2019-12-06 10:39:22 -0600

    add spring tutorial files to get project spun up

--- src/main/java/hello/Hello.java
+++
@@ -1,8 +0,0 @@
-package main.java.hello;
-
-public class Hello {
-    public static void main(String[] args) {
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
-    }
-}
